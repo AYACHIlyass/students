a2111fc890e094da55c3f149d67f4c6ebb9ee54a
iayach <ilyass.ayach@chanel.com>
2022-12-15 21:48:33 +0100

    add student details

--- src/routes/CustomRoutes.tsx
+++ src/routes/CustomRoutes.tsx
@@ -2,10 +2,12 @@ import {Route, Routes} from 'react-router';
 import {FC} from "react";
 import {Route as RouteModel} from "./model/Route";
 import StudentsList from "../components/studentsList/StudentsList";
+import StudentProfileDetails from "../components/studentProfileDetails/StudentProfileDetails";
 
 export const appRoutes: RouteModel[] = [
     {path: '/', component: <StudentsList/>},
     {path: '/studentList', component: <StudentsList/>},
+    {path: '/studentList/:studentId', component: <StudentProfileDetails/>},
 ];
 export const CustomRoutes: FC = () => {
     return <Routes>
--- src/service/StudentService.ts
+++ src/service/StudentService.ts
@@ -1,8 +1,17 @@
-import { Student } from "./../model/Student";
+import {Student} from "./../model/Student";
 import axios from "axios";
+
 export const fetchStudents = async (): Promise<Student[]> => {
-  const response = await axios.get<Promise<Student[]>>(
-    process.env.REACT_APP_BASE_URL!
-  );
-  return response.data;
+    const response = await axios.get(
+        process.env.REACT_APP_BASE_URL!
+    );
+    return response.data
+};
+
+export const fetchStudentById = async (studentId: string): Promise<Student> => {
+    const response = await axios.get(
+        `${process.env.REACT_APP_BASE_URL!}/${studentId}`
+    );
+
+    return response.data
 };
